c7faf79920fd2ce086171500838d52b11f181fa2
Nacho Coll <ignacio.coll@gmail.com>
2018-10-11 21:18:12 +0200

    #if DEBUG cannot be used on templates

--- templates/api-template/src/MyLambdas.cs
+++ templates/api-template/src/MyLambdas.cs
@@ -13,12 +13,6 @@ namespace MyAPI {
         
         /* A function that will get Lambda resources created (only) */
         [Cloudformation4dotNET.Lambda.LambdaResourceProperties(TimeoutInSeconds=20)]
-        public void Echo(string Input, ILambdaContext Context) => 
-        #if DEBUG 
-            /* for local testing */
-            Context.Logger.Log(Input.ToUpper()); 
-        #else  
-            LambdaLogger.Log(Input.ToUpper());
-        #endif       
+        public void Echo(string Input, ILambdaContext Context) => Context?.Logger?.Log(Input.ToUpper());
     }
 }
