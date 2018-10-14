ccbcc4ae270198d53b62fe2939dbc4ae89d7cbc4
Nacho Coll <ignacio.coll@gmail.com>
2018-10-14 18:37:42 +0200

    travis template initial

--- templates/api-template-travis/.deploy.sh
+++ templates/api-template-travis/.deploy.sh
@@ -19,10 +19,17 @@ set -e
 
 # test, staging or prod
 ENVIRONMENT=$1
+# the csproj file name
+CSPROJ_FILENAME=$2
+# assembly filename as mentioned at the .csproj
+ASSEMBLY_FILENAME=$3
+# a tag used to identify resources on AWS
+TAG_CODE=$4
 
-######################################
-# STEP 1: SET AWS Credentials        #
-######################################
+
+# STEP 1
+###################################################
+# SET AWS Credentials        
 
 AWS_ACCOUNT=$(case $ENVIRONMENT in test) echo $AWS_ACCOUNT_test ;; staging) echo $AWS_ACCOUNT_staging ;; prod) echo $AWS_ACCOUNT_prod ;; esac)
 AWS_ROLE=$(echo arn:aws:iam::$AWS_ACCOUNT:role/$AWS_DEPLOY_ROLE)
@@ -36,33 +43,37 @@ aws configure set default.region $AWS_DEFAULT_REGION
 aws configure set profile.deploy.role_arn $AWS_ROLE
 aws configure set profile.deploy.source_profile default
 
-######################################
-# STEP 2: BUILD ARTIFACTS            #
-######################################
+# STEP 2
+###################################################
+# BUILD ARTIFACTS            
 
 # csproj files
-PROJECT_FILE=$(echo $TRAVIS_BUILD_DIR/src/$CSPROJ_FILENAME.csproj)
+CSPROJ_FULLPATH=$(echo $TRAVIS_BUILD_DIR/src/$CSPROJ_FILENAME.csproj)
 
 # artifacts local output location
 ARTIFACT_PATH=$(echo $TRAVIS_BUILD_DIR/artifacts/)
 ARTIFACT_FILENAME=$(echo $ENVIRONMENT-$TRAVIS_BUILD_NUMBER.zip)
-ARTIFACT_FULLPATH=$(echo $TRAVIS_BUILD_DIR/artifacts/$ARTIFACT_FILENAME)
+ARTIFACT_FULLPATH=$(echo $ARTIFACT_PATH/$ARTIFACT_FILENAME)
 
 echo "creating artifact"
 # define code constants
-sed -i -e "s/RELEASE/${ENVIRONMENT^^}/g" $PROJECT_FILE
+sed -i -e "s/RELEASE/${ENVIRONMENT^^}/g" $CSPROJ_FULLPATH
 # publish dotnet code
 dotnet publish $TRAVIS_BUILD_DIR/src -o $ARTIFACT_PATH --framework netcoreapp2.1 --runtime linux-x64 -c Release
 # package code
 echo "packaging artifact"
 zip -j $ARTIFACT_FULLPATH $ARTIFACT_PATH/* 
 
-######################################
-# STEP 2: SEND ARTIFACTS TO S3       #
-######################################
+# results example: ./artifacts/prod-34.zip
+###################################################
+
+# STEP 3
+###################################################
+# SEND ARTIFACTS TO S3       
+
 
 # artifact location on AWS S3
-S3_BUCKET=$(echo deployments-$AWS_ACCOUNT)
+S3_BUCKET=$(echo cf4dotnet-$AWS_ACCOUNT)
 S3_ARTIFACT_BUCKET=$(echo s3://$S3_BUCKET/api)
 S3_ARTIFACT_URI=$(echo $S3_ARTIFACT_BUCKET/$ARTIFACT_FILENAME)
 
@@ -70,70 +81,40 @@ S3_ARTIFACT_URI=$(echo $S3_ARTIFACT_BUCKET/$ARTIFACT_FILENAME)
 echo "copying artifact to s3"
 aws s3 --profile deploy cp $ARTIFACT_FULLPATH $S3_ARTIFACT_URI
 
-#############################################
-# STEP 3: BUILD cloudformation templates    #
-#############################################
+# results example: s3://cf4dotnet-36634273/api/prod-34.zip
+############################################################################
 
+# STEP 4
+############################################################################
+# BUILD cloudformation templates    
 
 # cloudformation injection project
-INJECTION_DLL_FILE=$(echo $ARTIFACT_PATH/nwayapi.dll)
-INJECTION_FILESPATH=$(echo $TRAVIS_BUILD_DIR/deploy)
-INJECTION_PROJECT_FILE=$(echo $TRAVIS_BUILD_DIR/deploy/injection.csproj)
-
-# local cloudformation templates
-CF_DATABASE_TEMPLATE=$(echo $TRAVIS_BUILD_DIR/deploy/dynamodb.yml)
-CF_BASE_TEMPLATE=$(echo $TRAVIS_BUILD_DIR/sam-base.yml)
-CF_INJECTED_TEMPLATE=$(echo $TRAVIS_BUILD_DIR/deploy/sam-$ENVIRONMENT.yml)
-
-# SNS
-SNS_RESULTS_TOPIC=$(echo arn:aws:sns:us-east-1:$AWS_ACCOUNT:nway-deployments)
-# AWS cloudformation STACKS
-CF_DATABASE_STACKNAME=$(echo nway-api-dynamodb)
-CF_BASE_STACKNAME=$(echo nway-api)
-CF_INJECTED_STACKNAME=$(echo nway-api-$ENVIRONMENT)
-
-
-##############################################
-# dynamically create CF API templates
-##############################################
-
-# get the last deployed artifact for this deployment (lambda N-1 must reference previous code version!)
-# echo $S3_ARTIFACT_BUCKET
-#PREVIOUS_RELEASE_NUMBER=$(echo aws s3 --profile deploy ls $S3_ARTIFACT_BUCKET/$ARTIFACT_BASEFILENAME | awk '{print $4}' | sed 's/[^0-9]*//g' | sort -n | tail -1)
-# echo $PREVIOUS_RELEASE_NUMBER
+CF4DOTNET_SOURCE_DLL=$(echo $ARTIFACT_PATH/$ASSEMBLY_FILENAME.dll)
 
 echo "building injected template"
-dotnet run --project $INJECTION_PROJECT_FILE -- $INJECTION_DLL_FILE $INJECTION_FILESPATH $ENVIRONMENT $TRAVIS_BUILD_NUMBER
+dotnet cf4dotnet api $CF4DOTNET_SOURCE_DLL -b $TRAVIS_BUILD_NUMBER -e $ENVIRONMENT
+
+# results example: ./sam-base.yml and ./sam-prod.yml
+############################################################################
+
+# STEP 5
+############################################################################
+# deploy templates to AWS   
 
 
-##############################################
-# execute CF API templates
-##############################################
+CF_BASE_TEMPLATE=$(echo $TRAVIS_BUILD_DIR/sam-base.yml)
+CF_ENVIRONMENT_TEMPLATE=$(echo $TRAVIS_BUILD_DIR/sam-$ENVIRONMENT.yml)
+
+CF_BASE_STACKNAME=$(echo $TAG_CODE-base)
+CF_ENVIRONMENT_STACKNAME=$(echo $TAG_CODE-$ENVIRONMENT)
 
 # deploy base template
 echo "deploy CF base template"
-cat $CF_BASE_TEMPLATE
-aws cloudformation deploy --profile deploy --template-file $CF_BASE_TEMPLATE --stack-name $CF_BASE_STACKNAME --parameter-overrides PackageBaseFileName=$ARTIFACT_BASEFILENAME PackageVersion=$TRAVIS_BUILD_NUMBER S3Bucket=$S3_BUCKET --tags appcode=nway --no-fail-on-empty-changeset 
+aws cloudformation deploy --profile deploy --template-file $CF_BASE_TEMPLATE --stack-name $CF_BASE_STACKNAME --parameter-overrides PackageBaseFileName=$ENVIRONMENT PackageVersion=$TRAVIS_BUILD_NUMBER S3Bucket=$S3_BUCKET --tags appcode=$TAG_CODE --no-fail-on-empty-changeset 
 
 # deploy environment template
-echo "deploy CF injected template"
-cat $CF_INJECTED_TEMPLATE
-aws cloudformation deploy --profile deploy --template-file $CF_INJECTED_TEMPLATE --stack-name $CF_INJECTED_STACKNAME --tags appcode=nway --no-fail-on-empty-changeset 
+echo "deploy CF environment template"
+aws cloudformation deploy --profile deploy --template-file $CF_ENVIRONMENT_TEMPLATE --stack-name $CF_ENVIRONMENT_STACKNAME --tags appcode=$TAG_CODE --no-fail-on-empty-changeset 
 
-
-##############################################
-# integration tests
-##############################################
-echo "running tests"
-newman run $TEST_POSTMAN_FULLPATH -e $TEST_POSTMAN_ENVIRONMENT_FULLPATH --suppress-exit-code --reporters html --reporter-html-export $TEST_POSTMAN_RESULT_FULLPATH
-#upload the result to S3 (in case of success)
-echo "sending test results to S3"
-aws s3 --profile deploy cp $TEST_POSTMAN_RESULT_FULLPATH $S3_POSTMAN_RESULT_URI
-# 1 day access to report
-echo "building SNS topic message"
-echo -e "Deploy result for environment $ENVIRONMENT, travis build number $TRAVIS_BUILD_NUMBER\n" > "$TEST_POSTMAN_S3_PRESIGNED_URI_FULLPATH"
-echo "getting S3 test report presigned URI"
-aws s3 --profile deploy presign $S3_POSTMAN_RESULT_URI --expires-in 86400 >> "$TEST_POSTMAN_S3_PRESIGNED_URI_FULLPATH"
-# send top SNS topic
-echo "publishing test result to topic"
-aws sns --profile deploy publish --topic-arn $SNS_RESULTS_TOPIC --message file://$TEST_POSTMAN_S3_PRESIGNED_URI_FULLPATH
+# results: your code is on AWS!
+############################################################################
