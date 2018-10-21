f043e77cd561b0417d7df5eafc47d535018d09f3
Nacho Coll <ignacio.coll@gmail.com>
2018-10-21 10:44:29 +0200

    minors

--- templates/api-template-travis/.deploy.sh
+++ templates/api-template-travis/.deploy.sh
@@ -18,18 +18,18 @@ set -e
 #################
 
 # test, staging or prod
-ENVIRONMENT=${1:=prod}
+ENVIRONMENT=${1:-prod}
 # the csproj file name
-CSPROJ_FILENAME=${2:=MyProject}
+CSPROJ_FILENAME=${2:-MyProject}
 # assembly filename as mentioned at the .csproj
-ASSEMBLY_FILENAME=${3:=MyProjectAssemblyName}
+ASSEMBLY_FILENAME=${3:-nway.map.component.login.api}
 # a tag used to identify resources on AWS
-TAG_CODE=${4:=MyProjectTagCode}
+TAG_CODE=${4:-nway.map.component.login.api}
 
 
 # STEP 1
-###################################################
-echo "Setting AWS Credentials #####################"      
+######################################################################################################
+echo "Setting AWS Credentials ########################################################################"      
 
 AWS_ACCOUNT=$(case $ENVIRONMENT in test) echo $AWS_ACCOUNT_test ;; staging) echo $AWS_ACCOUNT_staging ;; prod) echo $AWS_ACCOUNT_prod ;; esac)
 AWS_ROLE=$(echo arn:aws:iam::$AWS_ACCOUNT:role/$AWS_DEPLOY_ROLE)
@@ -43,15 +43,15 @@ aws configure set profile.deploy.role_arn $AWS_ROLE
 aws configure set profile.deploy.source_profile default
 
 # STEP 2
-###################################################
-echo "Building artifact ###########################"            
+######################################################################################################
+echo "Building artifact ##############################################################################"            
 
 # csproj files
 CSPROJ_FULLPATH=$(echo $TRAVIS_BUILD_DIR/src/$CSPROJ_FILENAME.csproj)
 
 # artifacts local output location
 ARTIFACT_PATH=$(echo $TRAVIS_BUILD_DIR/artifacts/)
-ARTIFACT_FILENAME=$(echo $ENVIRONMENT-$TRAVIS_BUILD_NUMBER.zip)
+ARTIFACT_FILENAME=$(echo $CSPROJ_FILENAME-$TRAVIS_BUILD_NUMBER.zip)
 ARTIFACT_FULLPATH=$(echo $ARTIFACT_PATH/$ARTIFACT_FILENAME)
 
 # define code constants
@@ -65,36 +65,43 @@ zip -j $ARTIFACT_FULLPATH $ARTIFACT_PATH/*
 ###################################################
 
 # STEP 3
-###################################################
-echo "Sending artifact to AWS S3 ##################"     
+######################################################################################################
+echo "Sending artifact to AWS S3 #####################################################################"     
 
 # artifact location on AWS S3
 ARTIFACT_S3_BUCKET=$(echo cf4dotnet-$AWS_ACCOUNT)
-ARTIFACT_S3_KEY=$(echo MyAPI/cf4dotnet-$AWS_ACCOUNT/$ARTIFACT_FILENAME)
-ARTIFACT_S3_URI=$(ehco s3://$S3_BUCKET/$ARTIFACT_S3_KEY)
+ARTIFACT_S3_KEY=$(echo nway.map.component.login.api/$ARTIFACT_FILENAME)
+ARTIFACT_S3_URI=$(echo s3://$ARTIFACT_S3_BUCKET/$ARTIFACT_S3_KEY)
 
-# upload code to S3 deployment bucket
+# check for S3 bucket
+if  aws --profile deploy s3api list-buckets --output text | grep $ARTIFACT_S3_BUCKET; then
+    echo "Bucket found"
+else
+    echo "Creating the artifacts bucket"
+    aws --profile deploy s3api create-bucket --bucket $ARTIFACT_S3_BUCKET --region $AWS_DEFAULT_REGION
+fi
+
+# upload code to S3 bucket
 echo "copying $ARTIFACT_FULLPATH to $ARTIFACT_S3_URI"
 aws s3 --profile deploy cp $ARTIFACT_FULLPATH $ARTIFACT_S3_URI
 
-# example: s3://cf4dotnet-36634273/api/prod-34.zip
-###################################################
+# s3://$ARTIFACT_S3_BUCKET/$ARTIFACT_S3_KEY zip file is on  S3
+######################################################################################################
 
 # STEP 4
-###################################################
-echo "Building AWS Cloudformation templates #######"  
+######################################################################################################
+echo "Building AWS Cloudformation templates ##########################################################"  
 
 # cloudformation injection project
 CF4DOTNET_SOURCE_DLL=$(echo $ARTIFACT_PATH/$ASSEMBLY_FILENAME.dll)
+$TRAVIS_BUILD_DIR/tools/dotnet-cf4dotnet api $CF4DOTNET_SOURCE_DLL -b $TRAVIS_BUILD_NUMBER -e $ENVIRONMENT
 
-dotnet cf4dotnet api $CF4DOTNET_SOURCE_DLL -b $TRAVIS_BUILD_NUMBER -e $ENVIRONMENT
-
-# results: ./sam-base.yml and ./sam-prod.yml
-###################################################
+# $TRAVIS_BUILD_DIR/sam-base.yml and $TRAVIS_BUILD_DIR/sam-$ENVIRONMENT.yml created.
+######################################################################################################
 
 # STEP 5
-###################################################
-echo "deploying templates to AWS ##################" 
+######################################################################################################
+echo "deploying templates to AWS #####################################################################" 
 
 CF_BASE_TEMPLATE=$(echo $TRAVIS_BUILD_DIR/sam-base.yml)
 CF_ENVIRONMENT_TEMPLATE=$(echo $TRAVIS_BUILD_DIR/sam-$ENVIRONMENT.yml)
@@ -103,11 +110,11 @@ CF_BASE_STACKNAME=$(echo $TAG_CODE-base)
 CF_ENVIRONMENT_STACKNAME=$(echo $TAG_CODE-$ENVIRONMENT)
 
 # deploy base template
-echo "deploying base template ..."
+#echo "deploying base template ..."
 aws cloudformation deploy --profile deploy --template-file $CF_BASE_TEMPLATE --stack-name $CF_BASE_STACKNAME --parameter-overrides ArtifactS3Bucket=$ARTIFACT_S3_BUCKET  ArtifactS3BucketKey=$ARTIFACT_S3_KEY --tags appcode=$TAG_CODE --no-fail-on-empty-changeset 
 
 # deploy environment template
-echo "deploying $ENVIRONMENT template ..."
+#echo "deploying $ENVIRONMENT template ..."
 aws cloudformation deploy --profile deploy --template-file $CF_ENVIRONMENT_TEMPLATE --stack-name $CF_ENVIRONMENT_STACKNAME --tags appcode=$TAG_CODE --no-fail-on-empty-changeset 
 
 # results: your code is on AWS!
